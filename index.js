9c5c97750d230d1ce267c37e012af7d8ecb6d2e5
dvshah <shahdevansh2000@gmail.com>
2019-04-05 23:30:43 +0000

    added safe login method

--- index.js
+++ index.js
@@ -4,17 +4,71 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var qs = require('querystring');
 var path = require('path');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var crypto = require('crypto');
+
 var app = express();
 var expressWs = require('express-ws')(app);
 
 let arduino_switch = "OFF"; 
 let arduinoSocket;
 
-app.get('/', function(req, res){
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(session({
+  key: 'user_sid',
+  secret: 'somerandonstuffs',
+  resave: true,
+  saveUninitialized: false,
+}));
+
+var sessionChecker = (req, res, next) => {
+  if (req.session.user) {
+      res.redirect('/');
+  } else {
+      next();
+      //res.redirect('/login');
+  }    
+};
+
+var sessionCheckerHome = (req, res, next) => {
+  if (!req.session.user) {
+      res.redirect('/login');
+  } else {
+      next();
+      //res.redirect('/login');
+  }    
+};
+
+app.get('/', sessionCheckerHome, function(req, res){
     //arduinoSocket.send(arduino_switch);
     res.sendFile('views/home.html' , { root : __dirname});
     //res.sendFile('../views/home.html', {root: __dirname});
     //res.sendFile('./views/home.html');
+})
+
+let username = "devansh";
+let password = "b871e4d8aaeaac3cea157f22fa9721ad4f3650b9d37fda912780b9da550c4f0e";
+
+app.get('/login', sessionChecker, function(req, res){
+  res.sendFile('views/login.html', {root : __dirname});
+})
+
+app.post('/login',urlencodedParser,function(req, res){
+  let usr = req.body.name;
+  let passwd = req.body.password;
+  console.log(passwd);
+  passwd = crypto.createHash('sha256').update(passwd).digest('hex');
+  console.log(passwd);
+  if(usr===username && passwd==password)
+  {
+    req.session.user = username;
+    res.redirect('/');
+  }
+  else
+  {
+    res.redirect('/login');
+  }
 })
 
 app.ws('/arduino', function(ws, req) {
